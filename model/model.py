d4584e765542e1f6361da51bf5d871bd5efbdfc1
pl2599 <laichristopher99@yahoo.com>
2020-06-25 18:21:13 -0400

    added more preprocessing steps

--- model/model.py
+++ model/model.py
@@ -18,32 +18,47 @@ def remove_count_outlier(df):
                           df['count'].mean()) <= 3 * df['count'].std()]
     return new_df
 
+# Remove outliers for train data
 train = remove_count_outlier(train)
 
 def add_time_date(df):
     """Function to add new columns using the datetime column
 
     Args:
-        df ([pd dataframe]): Raw data of bike data
+        df ([pd dataframe]): Dataframe with datetime column
 
     Returns:
         [pd dataframe]: Returns dataframe after appending datetime columns
     """
     new_df = df
-    new_df.datetime = df['datetime'].apply(lambda x: 
+    new_df['datetime'] = df['datetime'].apply(lambda x: 
                                                   datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
     new_df['hour'] = df['datetime'].apply(lambda x: x.hour)
-    new_df['day'] = df['datetime'].apply(lambda x: x.day)
     new_df['month'] = df['datetime'].apply(lambda x: x.month)
     new_df['year'] = df['datetime'].apply(lambda x: x.year)
     
     return new_df
 
+def get_categ_dummies(df, categ_variables):
+    """Function to get all category dummy variables, dropping the first column
+
+    Args:
+        df ([pd dataframe]): Dataframe with category variables
+        categ_variables ([type]): list of category variables
+    """
+
+    dummy_df = pd.get_dummies(df, prefix=[var[0] for var in categ_variables], drop_first=True, columns=categ_variables)
+    print(dummy_df.columns)
+    # dummy_df = dummy_df.drop(categ_variables, axis=1)
+    return pd.concat([df, dummy_df], axis=1)
+
+
 
 def preprocess_data(df):
     """Function to pre-process the bike share data.
     Adds datetime columns
-    Normalize Data
+    Adds Dummy variables
+    Drops Unnecessary Columns
 
 
     Args:
@@ -52,8 +67,17 @@ def preprocess_data(df):
     Returns:
         [pd dataframe]: Dataframe after performing preprocessing
     """
+
+    categ_variables = ['weather', 'season', 'workingday', 'hour', 'month', 'year']
+    drop_variables = ['datetime', 'registered', 'casual', 'windspeed', 'temp']
+
     new_df = add_time_date(df)
+    new_df = get_categ_dummies(new_df, categ_variables)
+
+    new_df = new_df.drop(drop_variables, axis=1)
 
     return new_df
 
 train = preprocess_data(train)
+
+print(train.columns)
