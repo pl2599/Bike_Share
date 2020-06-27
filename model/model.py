07b3618bb09fc8ed0ad12788bfed0350f04b2161
pl2599 <laichristopher99@yahoo.com>
2020-06-27 14:18:32 -0400

    polished pipelines and added TargetRegressor

--- model/model.py
+++ model/model.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from sklearn.base import BaseEstimator, TransformerMixMin
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from skklearn.compose import TransformedTargetRegressor
 
 train = pd.read_csv("../data/train.csv")
 test = pd.read_csv("../data/test.csv")
@@ -22,38 +26,9 @@ def remove_count_outlier(df):
 # Remove outliers for train data
 train = remove_count_outlier(train)
 
-def add_time_date(df):
-    """Function to add new columns using the datetime column
-
-    Args:
-        df ([pd dataframe]): Dataframe with datetime column
-
-    Returns:
-        [pd dataframe]: Returns dataframe after appending datetime columns
-    """
-    new_df = df
-    new_df['datetime'] = df['datetime'].apply(lambda x: 
-                                                  datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-    new_df['hour'] = df['datetime'].apply(lambda x: x.hour)
-    new_df['month'] = df['datetime'].apply(lambda x: x.month)
-    new_df['year'] = df['datetime'].apply(lambda x: x.year)
-    
-    return new_df
-
-def get_categ_dummies(df, categ_variables):
-    """Function to get all category dummy variables, dropping the first column
-
-    Args:
-        df ([pd dataframe]): Dataframe with category variables
-        categ_variables ([type]): list of category variables
-    """
-
-    return pd.get_dummies(df, prefix=[var[0] for var in categ_variables], drop_first=True, columns=categ_variables)
-
-
 # Create a transformer class to create columns using datetime
-class DateTransformer(BaseEstimator, TransformerMixMin):
-    def fit(self, X, y=None):
+class DateTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
         return self
     
     def transform(self, x, y=None):
@@ -64,48 +39,72 @@ class DateTransformer(BaseEstimator, TransformerMixMin):
             y ([pd series], optional): outputs. Defaults to None.
 
         Returns:
-            [pd dataframe]: Transformed dataframe with additional columns
+            [pd DataFrame]: Transformed dataframe with additional columns
         """
         x_datetime = x.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-        return pd.dataframe({
+        return pd.DataFrame({
             'hour': x_datetime.apply(lambda x: x.hour),
             'month': x_datetime.apply(lambda x: x.month),
             'year': x_datetime.apply(lambda x: x.year),
         })
 
 
+class CustomTargetTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, y):
+        return self
 
-def preprocess_data(df):
-    """Function to pre-process the bike share data.
-    Adds datetime columns
-    Adds Dummy variables
-    Drops Unnecessary Columns
+    def transform(self, y):
+        y_ = y.copy()
+        y_ = np.log(y_)
+        return y
 
-
-    Args:
-        df ([pd dataframe]): Raw data of bike data
-
-    Returns:
-        [pd dataframe]: Dataframe after performing preprocessing
-    """
-
-    categ_variables = ['weather', 'season', 'workingday', 'hour', 'month', 'year']
-    drop_variables = ['datetime', 'registered', 'casual', 'windspeed', 'temp']
-
-    # Add datetime split columns
-    new_df = add_time_date(df)
-
-    # OH Encode Categ Variables
-    new_df = get_categ_dummies(new_df, categ_variables)
-
-    # Drop unnecessary Columns
-    new_df = new_df.drop(drop_variables, axis=1)
-
-    return new_df.drop('count', axis = 1), new_df['count']
+    def inverse_transform(self, y):
+        y_ = y.copy()
+        y_ = np.exp(y_)
+        return y_
 
 
-X_train, y_train = preprocess_data(train)
+# Transformer for datetime
+date_transformer = Pipeline(
+    steps = [
+        ('datetime', DateTransformer()),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ]
+)
 
+# Transformer for numerical columns
+num_cols = []
+num_transformer = 
+
+# Transformer for categoriacl columns
+categ_cols = ['weather', 'season', 'workingday']
+categ_transformer = OneHotEncoder()
+
+# Create a preprocessor transformer for columns
+preprocessor = ColumnTransformer(
+    transformers = [
+        ('datetime', date_transformer, 'datetime'),
+        ('num', num_transformer, num_cols),
+        ('cat', categ_transformer, categ_cols)
+    ]
+)
+
+
+# Create a pipeline to combine the model and preprocessor transformer
+
+pipeline = Pipeline(
+    steps = [
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ]
+)
+
+
+model = TransformedTargetRegressor(
+    regressor=pipeline,
+    func=np.log1p, 
+    inverse_func=np.expm1
+)
 
 
 
--- model/tempCodeRunnerFile.py
+++ model/tempCodeRunnerFile.py
@@ -1,9 +1,60 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-#from sklearn.base import BaseEstimator, TransformerMixMin
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
 
 train = pd.read_csv("../data/train.csv")
 test = pd.read_csv("../data/test.csv")
 
-print(train[['count', 'season']])
+def remove_count_outlier(df):
+    """Function to remove outliers that are beyond 3 standard deviation for counts
+
+    Args:
+        df ([pd dataframe]): Raw data of bike data
+
+    Returns:
+        [pd dataframe]: dataframe after removing outliers of the count column
+    """
+    new_df = df[abs(df['count'] - 
+                          df['count'].mean()) <= 3 * df['count'].std()]
+    return new_df
+
+# Remove outliers for train data
+train = remove_count_outlier(train)
+
+# Create a transformer class to create columns using datetime
+class DateTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, x, y=None):
+        """Creates columns based on datetime column
+
+        Args:
+            x ([pd series]): dataframe of x values
+            y ([pd series], optional): outputs. Defaults to None.
+
+        Returns:
+            [pd DataFrame]: Transformed dataframe with additional columns
+        """
+        x_datetime = x.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        return pd.DataFrame({
+            'hour': x_datetime.apply(lambda x: x.hour),
+            'month': x_datetime.apply(lambda x: x.month),
+            'year': x_datetime.apply(lambda x: x.year),
+        })
+
+
+
+# Transformer for datetime
+date_transformer = Pipeline(
+    steps = [
+        ('datetime', DateTransformer()),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ]
+)
+
+print(date_transformer.fit_transform(train['datetime']))
